9e8afa26c0d98eaff4247ca8395db299c1be7373
Thomas Geissl <thomas.geissl@gmail.com>
2024-06-04 08:20:06 +0200

    upgraded vite

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { execSync } from "child_process";
 
 // https://vitejs.dev/config/
-const commitHash = require("child_process")
-  .execSync("git rev-parse --short HEAD")
-  .toString();
+const commitHash = execSync("git rev-parse --short HEAD").toString();
+
 export default defineConfig({
   define: {
     COMMIT_HASH: JSON.stringify(commitHash),
